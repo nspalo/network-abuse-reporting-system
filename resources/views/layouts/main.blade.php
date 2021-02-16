8b4cde1febdd0de6dd806ce7c5edddd9ccc278a0
Noel Palo <noelspalo@gmail.com>
2021-02-16 19:47:41 +0800

    Remove unused plugin - alertify

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,7 +16,6 @@
     <link rel="stylesheet" type="text/css" href="/plugins/bootstrap/4.6.0/dist/css/bootstrap.min.css">
     <!-- Font-Awesome -->
     <link rel="stylesheet" type="text/css" href="/plugins/font-awesome/5.15.2/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="/plugins/alertifyjs/css/alertify.min.css" />
 
     <!-- Custom CSS Files -->
     <link rel="stylesheet" type="text/css" href="/css/style.css">
@@ -42,9 +41,7 @@
 <script>window.jQuery || document.write('<script type="text/javascript" src="https://getbootstrap.com/docs/4.0/js/vendor/jquery-slim.min.js"><\/script>')</script>
 <script type="text/javascript" src="/plugins/popper.js/1.16.1/umd/popper.min.js"></script>
 <script type="text/javascript" src="/plugins/bootstrap/4.6.0/dist/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="/plugins/alertifyjs/alertify.min.js"></script>
 <script type="text/javascript" src="/plugins/axios/dist/axios.min.js"></script>
-
 
 <!-- Custom JavaScript Files -->
 <script type="text/javascript" src="/js/common/auto-scale-container.js"></script>
